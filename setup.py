7b18fbd82d3bcbc04f19b024d6c27288facb3277
Hans Engel <engel@engel.uk.to>
2012-07-19 12:09:47 -0700

    update ref to script in setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author_email='engel@engel.uk.to',
     py_modules=['gaeunit_cli'],
     packages=['gaeunit_cli_support'],
-    scripts=['bin/gaeunit'],
+    scripts=['scripts/gaeunit'],
     url='https://github.com/hans/gaeunit-cli',
     license='LICENSE.md',
     description='Run GAEUnit tests from the command line')
